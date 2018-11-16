e521f87d11d5ce7b7e8fa7ede9b68c09370a16e4
Goryunov Anton <goryu-anton@yandex.ru>
2018-11-16 10:10:57 +0300

    another

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	$("ul").hide();
+	$("ul li:odd").css("background-color", "#efefef");
+	$("h3 span").click(function(){
+		$(this).parent().next().slideToggle();
+	});
+});
